b4cdc330d0f4682f3f7db54111b5c9e2aa634013
Amie Wei <amie.wei12@gmail.com>
2023-02-22 19:58:12 -0600

    testing process.env

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ mongoose.set("strictQuery", false);
 const dev_db_url = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.jnybsd5.mongodb.net/firebase-auth?retryWrites=true&w=majority`;
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-console.log(MONGO_PASSWORD);
+console.log(process.env.MONGO_PASSWORD);
 console.log(mongoDB);
 
 mongoose.connect(mongoDB, {
